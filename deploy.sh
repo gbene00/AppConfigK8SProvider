1f580a751b0133cd5e5cb71c1eaf9d6fbaf7bd37
gbene00 <165289683+gbene00@users.noreply.github.com>
2024-03-28 15:38:01 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,100 @@
+   #/bin/bash
+
+echo "Deploy APPConfig Cluster"
+RESOURCE_GROUP="appconfig"
+AKS_CLUSTER_NAME="aksappconfig"
+UAMI="appconfigmsi"
+SUBSCRIPTION=""
+LOCATION="EastUS"
+APPCFG_NAME="appconfig0001t"
+
+az group create -n $RESOURCE_GROUP --location $LOCATION
+
+echo "--->Create App Configuration"
+az appconfig create --location $LOCATION --name $APPCFG_NAME --resource-group $RESOURCE_GROUP --enable-public-network true --sku Free
+
+echo "Getting AppConfig Id"
+APPCFG_ID=$(az appconfig show -n $APPCFG_NAME -g $RESOURCE_GROUP --query 'id' -otsv)
+
+echo "Getting AppConfig Endpoint"
+APPCFG_ENDPOINT=$(az appconfig show -n $APPCFG_NAME -g $RESOURCE_GROUP --query endpoint -o tsv)
+
+echo "--->Create AKS Cluster"
+az aks create -n $AKS_CLUSTER_NAME -g $RESOURCE_GROUP --node-count 2 --network-plugin azure --network-policy calico --enable-workload-identity --enable-oidc-issuer
+
+echo "--->Create a Managed Identity to be used by AppConfig provider"
+az identity create --name $UAMI --resource-group $RESOURCE_GROUP
+
+export USER_ASSIGNED_IDENTITY_CLIENT_ID="$(az identity show --resource-group $RESOURCE_GROUP --name $UAMI --query 'clientId' -otsv)"
+echo $USER_ASSIGNED_IDENTITY_CLIENT_ID
+
+export AKS_OIDC_ISSUER="$(az aks show -n $AKS_CLUSTER_NAME -g $RESOURCE_GROUP --query "oidcIssuerProfile.issuerUrl" -otsv)"
+echo $AKS_OIDC_ISSUER
+
+echo "--->Create Federated Identity"
+az identity federated-credential create --resource-group $RESOURCE_GROUP --name apconfig_federated --identity-name $UAMI --issuer ${AKS_OIDC_ISSUER} --subject  system:serviceaccount:azappconfig-system:az-appconfig-k8s-provider --audience api://AzureADTokenExchange
+
+echo "Creating Role Assignments"
+az role assignment create --assignee $USER_ASSIGNED_IDENTITY_CLIENT_ID --scope $APPCFG_ID --role "App Configuration Data Reader"
+
+echo "AKS Get Credentials"
+az aks get-credentials -n $AKS_CLUSTER_NAME -g $RESOURCE_GROUP --overwrite
+
+echo "Inserting KV in AppConfig"
+
+az appconfig kv set -n $APPCFG_NAME --key Settings:FontColor --value Purple -y
+az appconfig kv set -n $APPCFG_NAME --key Settings:Messages --value "Hello World" -y
+
+echo "Installing AppConfig Plugin"
+helm install azureappconfiguration.kubernetesprovider oci://mcr.microsoft.com/azure-app-configuration/helmchart/kubernetes-provider --namespace azappconfig-system --create-namespace
+
+cat << EOF > ./appconfig.yaml
+apiVersion: azconfig.io/v1
+kind: AzureAppConfigurationProvider
+metadata:
+  name: appconfigurationprovider-sample
+spec:
+  endpoint: $APPCFG_ENDPOINT
+  target:
+    configMapName: appconfigmap
+    configMapData:
+      type: json
+      key: mysettings.json
+  auth:
+    workloadIdentity:
+      managedIdentityClientId: $USER_ASSIGNED_IDENTITY_CLIENT_ID
+EOF
+
+kubectl apply -f ./appconfig.yaml
+
+
+echo "Deploy Demo Application"
+
+cat << EOF > ./appconfigdemo.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: appconfigdemo
+  labels:
+    app: appconfigdemo
+spec:
+  containers:
+  - name: appconfig
+    image: nginx
+    ports:
+    - containerPort: 80
+    volumeMounts:
+    - name: config-volume
+      mountPath: /app
+  volumes:
+  - name: config-volume 
+    configMap: 
+      name: appconfigmap
+EOF
+
+kubectl apply -f ./appconfigdemo.yaml
+
+echo "Waiting for Pod Deployment"
+sleep 15
+
+kubectl exec -it appconfigdemo -- apt update
